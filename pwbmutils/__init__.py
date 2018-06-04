493b0ca4f120cd691d1fa93d8b412938593ffc00
Nick Janetos <njanetos@wharton.upenn.edu>
2018-06-03 21:25:37 -0400

    Remove reference to CodeComponent in init script

--- pwbmutils/__init__.py
+++ pwbmutils/__init__.py
@@ -5,4 +5,3 @@ from .interface_reader import InterfaceReader
 from .interface_writer import InterfaceWriter
 from .pwbm_task import PWBMTask
 from .example_task import ExampleTask
-from .component_code import ComponentCode
