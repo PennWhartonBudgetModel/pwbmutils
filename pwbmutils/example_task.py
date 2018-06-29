1031b0f4149348be705478ed6c876c8c7cb963fb
Nick Janetos <njanetos@wharton.upenn.edu>
2018-06-29 07:45:37 -0400

    Improve documentation for example task

--- pwbmutils/example_task.py
+++ pwbmutils/example_task.py
@@ -1,4 +1,4 @@
-"""Contains luigi class for interfacing with external component interfaces.
+"""Contains sample luigi task illustrating the use of PWBMTask.
 """
 
 __author__ = "Nick Janetos"
@@ -14,8 +14,9 @@ import pwbmutils
 # pylint: disable=E1101, E1136
 
 class ExampleTask(pwbmutils.PWBMTask):
-    """Cached requested interfaces locally.
+    """Example task illustrates the use of PWBM task.
     """
+
 
     def requires(self):
         """Requires the interface reader.
