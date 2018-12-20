e1c0a9cde6dd12c5316636ccbd225a94e68b5410
Nick Janetos <njanetos@wharton.upenn.edu>
2018-12-20 11:34:34 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import matplotlib
 
 setup(
     name="pwbmutils",
-    version="0.91",
+    version="0.92",
     description="Collection of cross-component utility functions",
     url="https://github.com/PennWhartonBudgetModel/Utilities",
     author="Penn Wharton Budget Model",
